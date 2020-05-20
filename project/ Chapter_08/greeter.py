da441f6b90712053a13d73e0947901013e34545f
Daniel <lindongyuan@126.com>
2020-05-20 17:21:30 +0800

    Hos

---
+++ project/ Chapter_08/greeter.py
@@ -0,0 +1,17 @@
+def greet_user(username):
+    '''显示简单问候'''
+    print("hello!" + username.title() + "!")
+
+greet_user("Jesse")
+
+
+
+def display_message(chapter):
+    print("I'm learning " + chapter + " !" )
+
+display_message("Chapter-8")
+
+
+def favorite_book(title):
+    print("One of my favorite books is " + title + "。")
+favorite_book("Alice in wonderland")
---
+++ project/ Chapter_08/pets.py
@@ -0,0 +1,19 @@
+def describe_pet(animal_type,pet_name):
+    print("\nI have a " +  animal_type + ".")
+    print("My " + animal_type + " 's name is " + pet_name.title() + ".")
+
+#位置实参
+describe_pet("hamster","harry")
+describe_pet("dog","Willie")
+
+#关键字实参
+describe_pet(animal_type="hamster",pet_name="harry")
+describe_pet(pet_name="Willie",animal_type="dog")
+
+#指定默认值
+#注意形参位置
+def describe_pet(pet_name,animal_type='dog'):
+    print("\nI have a " +  animal_type + ".")
+    print("My " + animal_type + " 's name is " + pet_name.title() + ".")
+
+describe_pet(pet_name="Willie")
