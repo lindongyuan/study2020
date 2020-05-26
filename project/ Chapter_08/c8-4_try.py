b59c6048d886ee55a565668352e8621f48ef93b8
Daniel <lindongyuan@126.com>
2020-05-26 20:27:35 +0800

    Hos

--- project/ Chapter_08/c8-4_try.py
+++ project/ Chapter_08/c8-4_try.py
@@ -1,12 +1,7 @@
-
-def show_magicians(magicians,magician_x):
-    while maggicians:
-        magigician = magicians.pop()
-        print("\n正在打印的魔术题是： " + magigician)
-        maggie_x.append(magigician)
-
 magicians = ['tom','tony','sam']
 maggie_x = []
 
-show_magicians(maggicicians,magician_x)
-print(magicians)
+def show_magicians(magicians,maggie_x):
+    magician_type = magicians.pop()
+    while magicians:
+        print
